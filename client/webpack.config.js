571eae40d1ab26b8b3c0085fc1e2dd0cfdfc918e
vairavan-ven <vairavan.ven@gmail.com>
2024-01-14 17:43:37 +1100

    added comments in webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,16 +18,19 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+       // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'JATE'
       }),
-    
+      
+      // Injects our custom service worker
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-
+      
+      // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
